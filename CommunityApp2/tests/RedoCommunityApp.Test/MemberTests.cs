1cb30574d2cb993ca2f6349298f9db778fc9654c
df1217 <dffrank1217@gmail.com>
2017-02-09 21:37:29 -0800

    commited tests

---
+++ CommunityApp2/tests/RedoCommunityApp.Test/MemberTests.cs
@@ -0,0 +1,47 @@
+﻿using RedoCommunityApp.Controllers;
+using RedoCommunityApp.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RedoCommunityApp.Test
+{
+    public class MemberTests
+    {
+
+        [Fact]
+        public void CanGetMembers()
+        {
+            //Arrange
+            FakeMemberRepository repo = new FakeMemberRepository();
+            MemberController controller = new MemberController(repo);
+
+            //Act
+            List<Member> members = controller.Index().ViewData.Model as List<Member>;
+
+            //Assert
+            Assert.Equal(repo.GetAllMembers()[0].Name, members[0].Name);
+            Assert.Equal(repo.GetAllMembers()[0].Email, members[0].Email);
+            Assert.Equal(repo.GetAllMembers()[1].Name, members[1].Name);
+            Assert.Equal(repo.GetAllMembers()[1].Email, members[1].Email);
+
+        }
+
+        [Fact]
+        public void CanGetMemberByEmail()
+        {
+            //Arrange
+            FakeMemberRepository repo = new FakeMemberRepository();
+            MemberController controller = new MemberController(repo);
+            string testEmail = "jdoe@fake.com";
+
+            //Act
+            Member member = controller.MemberByEmail(testEmail).ViewData.Model as Member;
+
+            //Assert
+            Assert.Equal(repo.GetMemberByEmail(testEmail).Email, member.Email);
+            Assert.Equal(repo.GetMemberByEmail(testEmail).Name, member.Name);
+
+
+        }
+    }
+}
---
+++ CommunityApp2/tests/RedoCommunityApp.Test/MessageTest.cs
@@ -0,0 +1,45 @@
+﻿using RedoCommunityApp.Controllers;
+using RedoCommunityApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedoCommunityApp.Test
+{
+    public class MessageTest
+    {
+        [Fact]
+
+        public void CanGetMessages()
+        {
+            //Arrange
+            FakeMessageRepository repo = new FakeMessageRepository();
+            MessageController controller = new MessageController(repo);
+
+            //Act
+            List<Message> messages = controller.Index().ViewData.Model as List<Message>;
+
+            //Assert
+            Assert.Equal(repo.GetAllMessages()[0].From.Name, messages[0].From.Name);
+            Assert.Equal(repo.GetAllMessages()[1].Date, messages[1].Date);
+        }
+        [Fact]
+        public void CanGetMessageByMember()
+        {
+            //Arrange
+            FakeMessageRepository repo = new FakeMessageRepository();
+            MessageController controller = new MessageController(repo);
+
+            Member John = new Member() { Name = "John Doe", Email = "jdoe@fake.com" };
+
+            //Act
+            List<Message> messages = controller.MessagesByMember(John).ViewData.Model 
+                as List<Message>;
+            //Act
+            Assert.Equal(repo.GetMessagesByMember(John)[0].Body, messages[0].Body);
+
+        }
+    }
+}
