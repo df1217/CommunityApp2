23c6bd1266a8f06a7cdffdde9a10c1e852f991a0
df1217 <dffrank1217@gmail.com>
2017-03-13 20:47:13 -0700

    lab8 without db publishing to azure properly

--- CommunityApp2/src/RedoCommunityApp/Migrations/20170313213152_azure.Designer.cs
+++ CommunityApp2/src/RedoCommunityApp/Migrations/20170313213152_azure.Designer.cs
@@ -8,9 +8,10 @@ using RedoCommunityApp.Repositories;
 namespace RedoCommunityApp.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20170313213152_azure")]
+    partial class azure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
